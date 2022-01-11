57c39f3d10772be58bb09297ac5553e8f45e22df
Thomas simons <thomassimons98@gmail.com>
2022-01-11 18:47:31 +0000

    Add project files.

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PartyInvites.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartyInvites.Controllers {
+    public class HomeController : Controller {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse) //the object passed in this method is of type guestreposonse because we said it was at the top of the view (Its expecting) and model binding matched the IDs of each field to a property of an object.
+        {
+            if (ModelState.IsValid)
+            {
+                Repository.AddResponse(guestResponse);
+                return View("Thanks", guestResponse);
+            } else
+            {
+                return View();
+            }
+        }
+
+        public ViewResult ListResponses()
+        {
+            return View(Repository.Responses.Where(r => r.WillAttend == true));
+        }
+    }
+}
---
+++ PartyInvites/Models/Repository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartyInvites.Models
+{
+    //in-memory collection of objects
+    public static class Repository
+    {
+        private static List<GuestResponse> responses = new List<GuestResponse>();
+        //static class - can not be instantiated
+        public static IEnumerable<GuestResponse> Responses => responses; 
+
+        public static void AddResponse(GuestResponse response)
+        {
+            responses.Add(response);
+        }
+
+        public static void DeleteResponse(GuestResponse response)
+        {
+            responses.Remove(response);
+        }
+
+
+    }
+}
+ 
